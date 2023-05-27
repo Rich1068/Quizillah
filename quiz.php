42945862c3e196ac9de625a114da8869bb1a22d3
Mark Anthony Soberano <soberano.markanthony@auf.edu.ph>
2023-05-27 16:37:56 +0800

    3rd css

--- quiz.php
+++ quiz.php
@@ -48,42 +48,41 @@ try {
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Quiz</title>
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Quiz</title>
+    </head>
+    <body>
 
-<h1>Analogy Questions</h1>
+        <h1>Analogy Questions</h1>
 
-<h3>Instructions</h3>
+        <h3>Instructions</h3>
 
-<p style="color: gray">
-    There is a certain relationship between two given words on one side of : : and one word is given on another side of : : while another word is to be found from the given alternatives, having the same relation with this word as the words of the given pair bear. Choose the correct alternative.
-</p>
+        <p style="color: gray">
+            There is a certain relationship between two given words on one side of : : and one word is given on another side of : : while another word is to be found from the given alternatives, having the same relation with this word as the words of the given pair bear. Choose the correct alternative.
+        </p>
 
 
-<h1>Question #<?php echo $question->getNumber(); ?></h1>
-<h2 style="color: blue"><?php echo $question->getQuestion(); ?></h2>
-<h4 style="color: blue">Choices</h4>
-<form method="POST" action="quiz.php">
-<input type="hidden" name="number" value="<?php echo $question->getNumber();?>" />
+        <h1>Question #<?php echo $question->getNumber(); ?></h1>
+        <h2 style="color: blue"><?php echo $question->getQuestion(); ?></h2>
+        <h4 style="color: blue">Choices</h4>
 
-<?php foreach ($question->getChoices() as $choice): ?>
+        <form method="POST" action="quiz.php">
+            <input type="hidden" name="number" value="<?php echo $question->getNumber();?>" />
+            <?php foreach ($question->getChoices() as $choice): ?>
 
-<input
-    type="radio"
-    name="answer"
-    value="<?php echo $choice->letter; ?>" />
-    <?php echo $choice->letter; ?>)
-<?php echo $choice->label; ?><br />
+                <input
+                    type="radio"
+                    name="answer"
+                    value="<?php echo $choice->letter; ?>" />
+                    <?php echo $choice->letter; ?>)
+                <?php echo $choice->label; ?><br />
 
-<?php endforeach; ?>
+            <?php endforeach; ?>
+                <input type="submit" value="Next">
+        </form>
 
-<input type="submit" value="Next">
-</form>
-
-</body>
+    </body>
 </html>
 
