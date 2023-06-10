2b45d8fa8730426568dcf579724a8aef2b6d55e7
Rich1068 <rs106848@gmail.com>
2023-06-10 10:05:16 +0800

    final final final

--- save_score.php
+++ save_score.php
@@ -12,6 +12,7 @@ $score = $manager->computeScore($_SESSION['answers']);
 $id = $_SESSION['user']['id']; 
 $result = Score::register($username, $quiz, $score, $id);
 $_SESSION['buttonClicked'] = true;
+
 header('Location: result.php');
 exit;
 ?>
